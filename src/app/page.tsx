33558ff0752a05666b2095ca2f1b8b5510dae7ed
Kimitri <kimitrimusic@gmail.com>
2024-11-24 12:45:12 -0300

    style: fix lint issues on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            I will be back soon!            
-          </li>
-        </ol>
-
-      </main>
-    </div>
-  );
+export default function Home(): JSX.Element {
+	return (
+		<div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+			<main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+				<ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
+					<li className="mb-2">I will be back soon!</li>
+				</ol>
+			</main>
+		</div>
+	)
 }
