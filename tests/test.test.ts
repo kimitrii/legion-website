3daaf5278f6c40a7cdd4f346f20c8d77594c0e4d
Kimitri <kimitrimusic@gmail.com>
2024-12-01 02:47:08 -0300

    chore: run lint on codebase

--- tests/test.test.ts
+++ tests/test.test.ts
@@ -1,9 +1,9 @@
 import { expect, test } from 'vitest'
 
 test('adds 1 + 2 to equal 3', () => {
-    const sum = (first: number, second: number) => {
-        return first + second
-    }
-    
-    expect(sum(1, 2)).toBe(3)
+	const sum = (first: number, second: number): number => {
+		return first + second
+	}
+
+	expect(sum(1, 2)).toBe(3)
 })
